fc86d21e8589812a020eed3047c5aadd04c972b3
Prince <mrprince0088@gmail.com>
2022-06-30 12:36:42 +0530

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,62 @@
+require(`dotenv`).config();
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
+
+module.exports = {
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
+    siteTitle: `Prince Singh`,
+    siteTitleAlt: `Prince Singh`,
+    siteHeadline: `Prince Singh's Portfolio`,
+    siteUrl: `https://github.com/mrprince88`,
+    siteDescription: `Prince Singh's Portfolio designed using Gatsby`,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    author: `mrprince88`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Prince Singh`,
+        short_name: `Prince`,
+        description: `Prince's Portfolio`,
+        start_url: `/`,
+        background_color: `#141821`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#f6ad55`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+  pathPrefix: "/portfolio"
+};
